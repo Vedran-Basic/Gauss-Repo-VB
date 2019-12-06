e0d564df7fddf46b465053169e46b362232ed5b0
Vedran-Basic <vedran.basic98@gmail.com>
2019-12-06 17:39:24 +0100

    Initial commit

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="ThePage">
+    <div class="Headline">
+      <h1>
+        A single user with ID: {{ $route.params.id }}
+      </h1>
+    </div>
+    <div class="Link">
+      <a href="..">
+        Go back
+      </a>
+    </div>
+    <div>
+
+    </div>
+  </div>
+
+    
+</template>
+
+
+<style>
+  .ThePage {
+    outline: solid 1px red;
+    margin-left: 5%; /* Što je preporučeno, upotrebljavati postotke, px ili nešto drugo? */
+    margin-right: 5%;
+    margin-top:70px;
+    margin-bottom:0 auto;
+    min-height:200vh;
+    
+  }
+  .Headline {
+    outline: solid 1px black;
+    margin-left: 50px;
+    margin-right:50px;
+    margin-top:50px; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 30vh;
+  }
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>
+      Users view
+    </h1>
+    <p>
+      <a href="..">Home</a> <!-- Je li href za root page ..? -->
+    </p>
+    <br />
+    <input type="text" v-model="userID">
+    <button @click="onLoadUser"> Load User</button>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        userID: ''
+      }
+    },
+    methods: {
+      onLoadUser() {
+        this.$router.push('/users/' + this.userID)
+      }
+    }
+  }
+
+</script>
