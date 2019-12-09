902c39537d3dad245e6fb91ec093de59e938953c
Vedran-Basic <vedran.basic98@gmail.com>
2019-12-09 13:07:16 +0100

    Proba sa vise divisiona

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -5,6 +5,41 @@
         A single user with ID: {{ $route.params.id }}
       </h1>
     </div>
+    <div class="Headline">
+      <h1>
+        A single user with ID: {{ $route.params.id }}
+      </h1>
+    </div>
+    <div class="Headline">
+      <h1>
+        A single user with ID: {{ $route.params.id }}
+      </h1>
+    </div>
+    <div class="Headline">
+      <h1>
+        A single user with ID: {{ $route.params.id }}
+      </h1>
+    </div>
+    <div class="Headline">
+      <h1>
+        A single user with ID: {{ $route.params.id }}
+      </h1>
+    </div>
+    <div class="Headline">
+      <h1>
+        A single user with ID: {{ $route.params.id }}
+      </h1>
+    </div>
+    <div class="Headline">
+      <h1>
+        A single user with ID: {{ $route.params.id }}
+      </h1>
+    </div>
+    <div class="Headline">
+      <h1>
+        A single user with ID: {{ $route.params.id }}
+      </h1>
+    </div>
     <div class="Link">
       <a href="..">
         Go back
@@ -24,19 +59,20 @@
     outline: solid 1px red;
     margin-left: 5%; /* Što je preporučeno, upotrebljavati postotke, px ili nešto drugo? */
     margin-right: 5%;
-    margin-top:70px;
     margin-bottom:0 auto;
+    margin-top: 0 auto;
     min-height:200vh;
     
   }
   .Headline {
     outline: solid 1px black;
-    margin-left: 50px;
-    margin-right:50px;
-    margin-top:50px; 
-    display: flex;
+    margin-left: 1%;
+    margin-right: 1%;
+    margin-top:1% ;
+    display:  compact  ;
     justify-content: center;
     align-items: center;
-    min-height: 30vh;
+    text-align: center;
+    max-width: 35%;
   }
 </style>
