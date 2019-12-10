672821b20c98c30cdb0b2df268aa9d41a9b0e702
Vedran-Basic <vedran.basic98@gmail.com>
2019-12-10 12:09:23 +0100

    Some minor changes - styles.css not working

--- components/header.vue
+++ components/header.vue
@@ -2,9 +2,10 @@
   <header>
     <nav>
       <ul>
-        <li> <a href="~"> Home </a></li>
+        <li> <a href=".."> Home </a></li>
         <li> <a href="/movies"> Movies </a> </li>
         <li> <a href="/about"> About </a></li>
+
       </ul>
     </nav>
   </header>
